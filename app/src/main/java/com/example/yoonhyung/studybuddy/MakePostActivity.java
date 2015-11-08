1cad5172f927fc02a7668a361cbb5a90d9f3dbde
Yoon Hyung Choi <yhc5@duke.edu>
2015-11-07 20:45:01 -0500

    starting make post

---
+++ app/src/main/java/com/example/yoonhyung/studybuddy/MakePostActivity.java
@@ -0,0 +1,27 @@
+package com.example.yoonhyung.studybuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
+/**
+ * Created by Yoonhyung on 11/7/15.
+ */
+public class MakePostActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makepost);
+
+    }
+
+
+
+    private void createStudyRoom() {
+
+    }
+}
+
+
